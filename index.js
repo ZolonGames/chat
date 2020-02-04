20da0740a5b7253a214446282459981f77c9c80c
Christopher Stewart <admin@zolongames.com>
2020-02-03 22:20:38 -0600

    Message retention and page formatting
    Added additional message retention (Up to 25 messages) that will load upon connection. Added form formatting.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
+var messages = [];
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -10,12 +11,18 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
+    io.emit('chat message', messages);
     socket.on('disconnect', function(){
         console.log('user disconnected');
         });
     socket.on('chat message', function(msg){
         console.log('message: ' + msg.text);
-        io.emit('chat message', msg);
+        messages.unshift(msg);
+        if (messages.length > 25)
+        {
+            messages.pop();
+        }
+        io.emit('chat message', messages);
     });
 })
 
